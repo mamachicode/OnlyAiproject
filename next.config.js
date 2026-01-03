e001d6bd30cbc97e91e7ee17d38aaaacdddbd447
Mamachi <kudo75@hotmail.com>
2026-01-03 11:52:38 -0500

    Lock Vercel App Router root to src/app (canonical bind)

--- next.config.js
+++ next.config.js
@@ -3,5 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-};
-module.exports = nextConfig;
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  webpack(config) {
+    // HARD BIND APP ROUTER ROOT
+    config.resolve.alias['@/app'] = require('path').resolve(__dirname, 'src/app')
+    config.resolve.alias['@/components'] = require('path').resolve(__dirname, 'src/components')
+    config.resolve.alias['@/lib'] = require('path').resolve(__dirname, 'src/lib')
+    return config
+  },
+}
+
+module.exports = nextConfig
