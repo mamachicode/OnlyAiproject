bf78db6c2871d09834ba0ab9076ad25cddede637
Mamachi <kudo75@hotmail.com>
2026-01-05 10:44:56 -0500

    Canonicalize single Next root for Vercel stability

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-  webpack(config) {
-    // HARD BIND APP ROUTER ROOT
-    config.resolve.alias['@/app'] = require('path').resolve(__dirname, 'src/app')
-    config.resolve.alias['@/components'] = require('path').resolve(__dirname, 'src/components')
-    config.resolve.alias['@/lib'] = require('path').resolve(__dirname, 'src/lib')
-    return config
-  },
-}
+  reactStrictMode: true,
 
-module.exports = nextConfig
+  turbopack: {},
+
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
+};
+
+module.exports = nextConfig;
