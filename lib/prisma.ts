59e0631f2a41113d083120a109f5359c7428b50e
Mamachi <kudo75@hotmail.com>
2026-01-06 15:08:31 -0500

    Fix Prisma 7 runtime client import

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client/runtime/library"
 import { PrismaPg } from "@prisma/adapter-pg"
 
 const globalForPrisma = globalThis as any
