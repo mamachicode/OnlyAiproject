e85a672c543b4d229290b46bced2792c88158a22
Ralph <kudo75@hotmail.com>
2026-05-01 02:48:57 -0400

    Refine OnlyAi homepage CTA copy

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function HomePage() {
                 href="/login"
                 className="rounded-full border border-white/10 bg-white/10 px-5 py-2 hover:bg-white/15"
               >
-                Start
+                Create
               </Link>
             </div>
           </nav>
